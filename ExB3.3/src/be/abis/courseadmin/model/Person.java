d0a161525256571cba3c6fa8fa1e1f97d9ddf4c8
Sandy Schillebeeckx <sschillebeeckx@abis.be>
2022-09-13 15:08:21 +0200

    EXB3

---
+++ ExB3.3/src/be/abis/courseadmin/model/Person.java
@@ -0,0 +1,102 @@
+package be.abis.courseadmin.model;
+
+/**
+ * This is the person class. Each person has a firstName, lastName and a list of hobbies. A person is optionnally linked to a company.
+ * @author Sandy
+ **/
+public class Person {
+
+    private static int counter;
+    private int personNumber;
+    private String firstName;
+    private String lastName;
+    private Company company;
+    private String[] hobbies=new String[10];
+    private int hobbiesIndex;
+
+    public Person(String firstName, String lastName) {
+        personNumber=++counter;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Person(String firstName, String lastName, Company company) {
+        this(firstName, lastName);
+        this.company = company;
+    }
+
+    public static int getCounter() {
+        return counter;
+    }
+
+  /*  public static void setCounter(int counter) {
+        Person.counter = counter;
+    }*/
+
+    public int getPersonNumber() {
+        return personNumber;
+    }
+
+   /* public void setPersonNumber(int personNumber) {
+        this.personNumber = personNumber;
+    }*/
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public String[] getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(String[] hobbies) {
+        this.hobbies = hobbies;
+    }
+
+    public void addHobby(String hobby){
+        hobbies[hobbiesIndex]=hobby;
+        hobbiesIndex++;
+    }
+
+    /** adding hobbies based on a vararg**/
+    public void addHobbies(String... hobbies){
+        for (String hobby:hobbies){
+            this.addHobby(hobby);
+        }
+    }
+
+    /** Prints all the person's information**/
+    public void printInfo(){
+        Company c = company;
+        System.out.println("Person " + personNumber + ": " +firstName + " " + lastName + ((c!=null)?" works for " + c.getName() +"." : " is not linked to a company for the moment."));
+    }
+
+    /** A person can follow a course. The method uses the calculatePrice() method.
+     *
+     * @param c
+     * @see Course
+     */
+    public void attendCourse(Course c){
+        System.out.println(firstName + " is attending a " + c.getTitle() + " course.");
+    }
+}
---
+++ ExB3.3/src/be/abis/courseadmin/test/Test.java
@@ -0,0 +1,48 @@
+package be.abis.courseadmin.test;
+
+import be.abis.courseadmin.model.Company;
+import be.abis.courseadmin.model.Course;
+import be.abis.courseadmin.model.Person;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println("------------------------Course----------------------------------");
+        Course c = new Course("Java",5,500);
+        c.printInfo();
+       // c.calculateTotalPrice();
+
+        System.out.println("\n------------------------Company----------------------------------");
+        Company comp1 = new Company("Abis");
+        comp1.printInfo();
+
+        System.out.println("\n------------------------Persons----------------------------------");
+        Person p1 = new Person("Mary","Jones",comp1);
+        Person p2=new Person("John","Doe");
+        Person p3 = new Person("Bob","Janssens",new Company("Smals"));
+        Person[] persons = {p1,p2,p3};
+        System.out.println("There were " + Person.getCounter() + " persons created:");
+        for (Person p: persons){
+            p.printInfo();
+        }
+
+        System.out.println("\n------------------------Course with reduction----------------------------------");
+        double reductionPrice = c.calculateTotalPrice(10);
+        System.out.println("The reduced price is " + reductionPrice +".");
+
+        System.out.println("\n------------------------Follow course----------------------------------");
+        p3.attendCourse(c);
+
+        System.out.println("\n------------------------Hobbies----------------------------------");
+        p1.addHobby("soccer");
+        p2.addHobbies("gaming","walking","cooking");
+        p3.addHobbies("soccer","dancing");
+        for(Person p:persons){
+            System.out.println(p.getFirstName() + "'s hobbies are:");
+            for (String hobby: p.getHobbies()){
+                if(hobby!=null) System.out.println(hobby);
+            }
+        }
+    }
+}

081a889501e8ba54eaa998ae7358ab3d76d448da
Sandy Schillebeeckx <sschillebeeckx@abis.be>
2022-09-13 14:14:55 +0200

    ExB1.1

---
+++ ExB1.1/src/ExB11.java
@@ -0,0 +1,14 @@
+public class ExB11 {
+
+    public static void main(String[] args) {
+        String firstName="Sandy";
+        int age=44;
+        double monthlyGrossSalary=4321.98;
+        boolean senior=true;
+
+        System.out.println(firstName + " is " + age + " years old. Monthly gross salary is " + monthlyGrossSalary + ". Senior:"+senior);
+
+    }
+
+
+}

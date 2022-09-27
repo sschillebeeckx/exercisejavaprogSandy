4199fb57cb85f0d9c816981066ca6d79660f0da1
Sandy Schillebeeckx <sschillebeeckx@abis.be>
2022-09-27 16:06:51 +0200

    DecimalUtils changed

--- ExB7.2/src/be/abis/courseadmin/util/DecimalUtils.java
+++ ExB7.2/src/be/abis/courseadmin/util/DecimalUtils.java
@@ -1,12 +1,20 @@
 package be.abis.courseadmin.util;
 
 import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 
 public class DecimalUtils {
 
-    static DecimalFormat df = new DecimalFormat("#,##0.00");
+    static DecimalFormat setUpFormat(){
+        DecimalFormatSymbols otherSymbols = new DecimalFormatSymbols();
+        otherSymbols.setDecimalSeparator(',');
+        otherSymbols.setGroupingSeparator('.');
+        return new DecimalFormat("#,##0.00",otherSymbols);
+    }
+
 
     public static String format(double d){
+       DecimalFormat df = DecimalUtils.setUpFormat();
        return df.format(d);
     }
 }
